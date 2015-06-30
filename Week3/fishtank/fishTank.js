5162b0865440fcefae4c6727cc675ef5ec04a2aa
fionaRowan <fmr2112@columbia.edu>
2015-06-29 21:11:11 -0500

    writing starfish

--- Week3/fishtank/fishTank.js
+++ Week3/fishtank/fishTank.js
@@ -3,8 +3,6 @@
 // Project prompt: write code that randomly generates an image of 
 // a fish tank (using khan academy template below)
 // https://www.khanacademy.org/computing/computer-programming/programming/functions/p/project-fish-tank
-
-
 background(89, 216, 255);
 
 //fish variables
@@ -35,7 +33,7 @@ var drawFish = function( x, y, l, h, c){
          centerX-bodyLength/2-tailWidth, centerY+tailHeight);
     // eye
     fill(33, 33, 33);
-    ellipse(centerX+bodyLength/4, centerY, bodyHeight/5, bodyHeight/5);
+    ellipse(centerX+bodyLength/4, centerY, bodyHeight/5,          bodyHeight/5);
        
 };
 
@@ -57,12 +55,27 @@ drawKelp(kX, kY, kLength, kHeight, kColor);
 
 
 //draw starfish 
+var angle = 0; 
+var sX = random(150,300);
+var sY = random(100,300);
+var sLength = random(50,100);
+var sHeight = random(100,300);
+var sColor = color(random(0,162),random(0,200),random(0,255));
 
 
+var drawStarfish = function( x, y, l, h, c){
+    
+    rotate(angle); 
+    ellipse(x, y, l, h); 
+    angle+=2*3.1415/5;
+    rotate(angle); 
+    ellipse(x, y, l, h); 
+       
+};
+
+noStroke();
+fill(sColor);
+drawStarfish(sX,sY,sLength, sHeight, sColor);
 
 
-
-
-
-         
          
